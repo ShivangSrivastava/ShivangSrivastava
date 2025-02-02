c3f875d4efd0bab297608cd889dde99ed482287b
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-02-03 05:22:04 +0530

    update py

--- main.py
+++ main.py
@@ -7,17 +7,13 @@ FILE_NAME = "terminal.svg"
 doc = minidom.parse(FILE_NAME)
 
 
-# Get the current date and time in the desired format
 new_datetime = datetime.now().strftime("%a %b %d %H:%M:%S %Y on tty1")
 
-# Find all <tspan> elements and search for the one with the id "last-login"
 tspans = doc.getElementsByTagName('tspan')
 
 for tspan in tspans:
     if tspan.getAttribute('id') == 'last-login':
-        # Update the text content inside the <tspan> element
         tspan.firstChild.nodeValue = new_datetime
 
-# Save the modified SVG back to a file
 with open(FILE_NAME, 'w') as file:
     doc.writexml(file)
